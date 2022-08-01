c1f69d189cefc314b66ef115d47355c74a7de95d
Cliff Rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-07-31 22:53:20 -0700

    added JavaScript file for side nav functions

---
+++ assets/js/init.js
@@ -0,0 +1,9 @@
+// loads function for side navigation bar
+// only on 'mobile' width screens
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
